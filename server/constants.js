431981be93b16a31f47840c195502b040879a472
Devashish Rathod <rathod.devashish09@gmail.com>
2025-10-08 00:33:47 +0530

    subscripton done

--- server/constants.js
+++ server/constants.js
@@ -9,4 +9,27 @@ module.exports = {
     ANDROID: "android",
     IOS: "ios",
   }),
+
+  SUBSCRIPTION_TYPES: Object.freeze({
+    WEEKLY: "weekly",
+    MONTHLY: "monthly",
+    QUATERLY: "quarterly",
+    HALF_YEARLY: "half_yearly",
+    YEARLY: "yearly",
+  }),
+
+  DURATION_MAP: Object.freeze({
+    weekly: 7,
+    monthly: 30,
+    quarterly: 90,
+    half_yearly: 180,
+    yearly: 365,
+  }),
+
+  SUBSCRIPTION_PLANS: Object.freeze({
+    FREE: "free",
+    BASIC: "basic",
+    PREMIUM: "premium",
+    Family: "family",
+  }),
 };
